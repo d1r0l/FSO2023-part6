9e0e7614045caa7f85d312ff0e9b124116f1a0f4
d1r0l <xd1r0lx@gmail.com>
2023-07-18 01:25:40 +0400

    coursedata update up to ex 6.3

--- notes_frontend/src/App.js
+++ notes_frontend/src/App.js
@@ -1,31 +1,11 @@
-import { toggleImportanceOf } from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
 import NewNote from './components/NewNote'
+import Notes from './components/Notes'
 
 const App = () => {
-  const dispatch = useDispatch()
-  // eslint-disable-next-line react-redux/useSelector-prefer-selectors
-  const notes = useSelector(state => state)
-  // const importantNotes = useSelector(state => state.filter(note => note.important))
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id))
-  }
-
   return (
     <div>
-      <NewNote/>
-      <ul>
-        {notes.map(note =>
-          <li
-            key={note.id}
-
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   )
 }
