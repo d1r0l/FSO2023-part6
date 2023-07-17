2f535874e5a9a8583999c58d6231c06d4c14207a
d1r0l <xd1r0lx@gmail.com>
2023-07-17 12:41:26 +0400

    base counter

--- counter_frontend/src/App.js
+++ counter_frontend/src/App.js
@@ -1,7 +1,27 @@
+import React from 'react'
+import { useState } from 'react'
+
 function App() {
+  const [ counter, setCounter ] = useState(0)
+
+  const handlePlusClick = () => {
+    setCounter(counter + 1)
+  }
+
+  const handleMinusClick = () => {
+    setCounter(counter - 1)
+  }
+
+  const handleZeroClick = () => {
+    setCounter(0)
+  }
+
   return (
     <div>
-      <h1>App</h1>
+      <p>{counter}</p>
+      <button onClick={handlePlusClick}>plus</button>
+      <button onClick={handleMinusClick}>minus</button>
+      <button onClick={handleZeroClick}>zero</button>
     </div>
   )
 }
