6d3d84004f92139e712dc9b863780da5d890dada
d1r0l <xd1r0lx@gmail.com>
2023-07-20 19:54:47 +0400

    coursedata update up to ex 6.20

--- notes_query/src/App.js
+++ notes_query/src/App.js
@@ -5,8 +5,9 @@ const App = () => {
   const queryClient = useQueryClient()
 
   const newNoteMutation = useMutation(createNote, {
-    onSuccess: () => {
-      queryClient.invalidateQueries('notes')
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData('notes')
+      queryClient.setQueryData('notes', notes.concat(newNote))
     },
   })
 
@@ -18,8 +19,13 @@ const App = () => {
   }
 
   const updateNoteMutation = useMutation(updateNote, {
-    onSuccess: () => {
-      queryClient.invalidateQueries('notes')
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData('notes')
+      queryClient.setQueryData('notes', notes.map(note =>
+        note.id === newNote.id
+          ? newNote
+          : note
+      ))
     },
   })
 
@@ -27,7 +33,9 @@ const App = () => {
     updateNoteMutation.mutate({ ...note, important: !note.important })
   }
 
-  const result = useQuery('notes', getNotes)
+  const result = useQuery('notes', getNotes, {
+    refetchOnWindowFocus: false
+  })
 
   if ( result.isLoading ) {
     return <div>loading data...</div>
