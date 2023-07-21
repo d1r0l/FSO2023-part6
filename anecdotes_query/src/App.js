28145c7cba51fd33c735c1201288c8f006fa900b
d1r0l <xd1r0lx@gmail.com>
2023-07-21 17:19:01 +0400

    completed ex 6.20

--- anecdotes_query/src/App.js
+++ anecdotes_query/src/App.js
@@ -1,20 +1,28 @@
+/* eslint-disable no-unused-vars */
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { getAnecdotes } from './requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
-
-  // eslint-disable-next-line no-unused-vars
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      'content': 'If it hurts, do it more often',
-      'id': '47145',
-      'votes': 0
-    },
-  ]
+  const result = useQuery('anecdotes', getAnecdotes, {
+    retry: 2,
+    refetchOnWindowFocus: false
+  })
+
+  console.log(result)
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  } else if ( result.isError ) {
+    return <div>anecdote service unavalable due to problems in server</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
